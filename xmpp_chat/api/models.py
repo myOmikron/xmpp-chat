89bad9f1bdc4bbc1f7af94db4c96b43ed4bda0d5
myOmikron <accounts@omikron.dev>
2021-02-25 17:09:31 +0100

    Added RoomModel

--- xmpp_chat/api/models.py
+++ xmpp_chat/api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.db.models import CharField
 
-# Create your models here.
+
+class RoomModel(models.Model):
+    room_jid = CharField(default="", max_length=255, unique=True)
+    callback_uri = CharField(default="", max_length=255)
+    callback_secret = CharField(default="", max_length=255)
