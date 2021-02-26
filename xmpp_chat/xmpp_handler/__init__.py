910a28d4ad171b34aedf90c3b060982a56acd089
gammelalf <31348226+gammelalf@users.noreply.github.com>
2021-02-26 16:09:34 +0100

    Moved on_message handler out of xmpp client and fixed stuff

--- xmpp_chat/xmpp_handler/__init__.py
+++ xmpp_chat/xmpp_handler/__init__.py
@@ -1,18 +1,40 @@
+import asyncio
+import hashlib
+import json
+import os
 import time
+import logging
+from datetime import datetime
+from queue import Queue
 from threading import Thread
 
+import requests
 from django.apps import apps
+from django.conf import settings
 from django.core.exceptions import MiddlewareNotUsed, AppRegistryNotReady
 
 from xmpp_handler.xmpp import XmppHandler
 from xmpp_handler.state import State
 
 
+logger = logging.getLogger(__name__)
+
+
 class XmppStartupMiddleware:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+        logging.basicConfig(level=logging.INFO)
+
+        asyncio.set_event_loop(asyncio.new_event_loop())
         XmppHandler.instance = XmppHandler()
+        XmppHandler.instance.on_message = on_message
         XmppHandler.instance.start_in_thread()
+
+        threads = [RequestThread() for i in range(1)]
+        for thread in threads:
+            thread.start()
+
         StateLoader().start()
+
         raise MiddlewareNotUsed("Good.Design")
 
 
@@ -25,3 +47,51 @@ class StateLoader(Thread):
                 break
             except AppRegistryNotReady:
                 time.sleep(1)
+
+
+def on_message(room_jid, message, member, source, **kwargs):
+    if member.nick == settings.XMPP_USER_NICK:
+        return
+    else:
+        user = member.nick
+        text = message.body.any()
+
+        try:  # TODO Experimental
+            timestamp = message.xep0203_delay[0].stamp
+            logger.info("Skipping old message")
+            return
+        except IndexError:
+            pass
+
+        room = State.instance.get(room_jid)
+        if not room or not room.callback_secret or not room.callback_uri:
+            return
+
+        params = {
+            "user_name": user,
+            "message": text
+        }
+
+        params["checksum"] = hashlib.sha512((
+                "sendChatMessage"
+                + json.dumps(params)
+                + room.callback_secret
+                + str(int(datetime.now().timestamp()))
+        ).encode("utf-8")).hexdigest()
+
+        RequestThread.queue.put((room.callback_uri, json.dumps(params)))
+
+
+class RequestThread(Thread):
+
+    queue = Queue()
+    running: bool
+
+    def run(self):
+        self.running = True
+        while self.running:
+            uri, data = self.queue.get()
+            requests.post(uri, data=data)
+
+    def stop(self):
+        self.running = False
--- xmpp_chat/xmpp_handler/xmpp.py
+++ xmpp_chat/xmpp_handler/xmpp.py
@@ -8,8 +8,11 @@ import aioxmpp
 from django.conf import settings
 
 
-logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
+
+
+def replace_me(*args, **kwargs):
+    raise NotImplementedError()
 
 
 class XmppHandler:
@@ -17,35 +20,31 @@ class XmppHandler:
     instance: "XmppHandler" = None
 
     thread: Thread
-    _loop: asyncio.AbstractEventLoop
-    msg_queue: queue.Queue
+    queue: queue.Queue
 
     jid: aioxmpp.JID
     client: aioxmpp.Client
     muc: aioxmpp.MUCClient
 
     def __init__(self):
-        self.msg_queue = queue.Queue()
+        self.queue = queue.Queue()
+        self.on_message = replace_me
 
         self.jid = aioxmpp.JID.fromstr(settings.XMPP_USER_JID)
         self.client = aioxmpp.Client(self.jid, aioxmpp.make_security_layer(settings.XMPP_USER_PASS))
         self.muc = self.client.summon(aioxmpp.MUCClient)
 
         self.rooms = {}
-        self.add_room("d49579fc-0f89-4a6a-98dd-a86282edee54@conference.stream.zfn.schule")
-
-    @property
-    def loop(self):
-        if not self._loop:
-            self._loop = asyncio.get_event_loop()
-        return self._loop
 
     def add_room(self, jid: str):
+        self.queue.put(("JOIN", (jid,)))
+
+    def _add_room(self, jid: str):
         room, future = self.muc.join(aioxmpp.JID.fromstr(jid), settings.XMPP_USER_NICK)
         room.future = future
 
         def callback(f):
-            room.on_message.connect(partial(self._on_message, jid))
+            room.on_message.connect(partial(self.on_message, jid))
 
         future.add_done_callback(callback)
         self.rooms[jid] = room
@@ -53,27 +52,10 @@ class XmppHandler:
     def remove_room(self, jid):
         return NotImplemented
 
-    def _on_message(self, room_jid, message, member, source, **kwargs):
-        if member.nick == settings.XMPP_USER_NICK:
-            return
-        else:
-            user = member.nick
-            text = message.body.any()
-
-            try:  # TODO Experimental
-                timestamp = message.xep0203_delay[0].stamp
-                logger.debug("Skipping old message")
-                return
-            except:
-                pass
-
-            # TODO api call
-            print(text)
-
     def send_message(self, room_jid: str, msg: str):
-        self.msg_queue.put((room_jid, msg))
+        self.queue.put(("SEND", (room_jid, msg)))
 
-    async def _send_to_room(self, room_jid: str, msg: str):
+    async def _send_message(self, room_jid: str, msg: str):
         room = self.rooms[room_jid]
         await room.future
         stanza = aioxmpp.Message(
@@ -87,17 +69,20 @@ class XmppHandler:
         async with self.client.connected():
             while True:
                 try:
-                    room, msg = self.msg_queue.get(block=False)
-                    await self._send_to_room(room, msg)
+                    type_, data = self.queue.get(block=False)
+                    if type_ == "SEND":
+                        await self._send_message(*data)
+                    elif type_ == "JOIN":
+                        self._add_room(*data)
                 except queue.Empty:
                     await asyncio.sleep(0)
 
     def start_in_thread(self):
-        self._loop = asyncio.get_event_loop()
+        loop = asyncio.get_event_loop()
 
         def run():
-            asyncio.set_event_loop(self._loop)
-            self._loop.run_until_complete(self.run())
+            asyncio.set_event_loop(loop)
+            loop.run_until_complete(self.run())
 
         self.thread = Thread(target=run)
         self.thread.start()
